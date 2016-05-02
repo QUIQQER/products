3cda2bfe96a8a32e5c2acceacf2402dba16aca60
Patrick Müller <p.mueller@pcsg.de>
2016-05-02 14:25:56 +0200

    clear search cache ajax

---
+++ ajax/search/clearSearchCache.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_products_ajax_search_clearSearchCache
+ */
+
+use \QUI\ERP\Products\Search\Cache;
+
+/**
+ * Clear the search cache for all cached search values
+ *
+ * @return array
+ */
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_products_ajax_search_clearSearchCache',
+    function () {
+        // TODO: permission?
+        Cache::clear();
+    },
+    array(),
+    'Permission::checkAdminUser'
+);
