e7ac515ba93dfb55dc20addf7af7133f7a753fb0
Patrick Müller <p.mueller@pcsg.de>
2016-05-02 14:11:56 +0200

    ajax funktion für suchdaten backend

---
+++ ajax/search/backend/getSearchFieldData.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_products_ajax_search_backend_getSearchFields
+ */
+
+use \QUI\ERP\Products\Handler\Search as SearchHandler;
+
+/**
+ * Get all fields that are available for search for a specific Site
+ *
+ * @return array
+ */
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_products_ajax_search_backend_getSearchFieldData',
+    function () {
+        $BackEndSearch = SearchHandler::getBackendSearch();
+        return $BackEndSearch->getSearchFieldData();
+    },
+    array(),
+    'Permission::checkAdminUser'
+);
