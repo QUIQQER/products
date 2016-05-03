d5cd5223ebd5314d1e73832b5b1e05c623bf82de
Patrick Müller <p.mueller@pcsg.de>
2016-05-03 14:29:50 +0200

    tag generierungs konsolen tool; such ausführung ajax

---
+++ ajax/search/backend/execute.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_products_ajax_search_backend_execute
+ */
+
+use \QUI\ERP\Products\Handler\Search as SearchHandler;
+
+/**
+ * Get all fields that are available for search for a specific Site
+ *
+ * @param array $searchData
+ * @return array - product ids
+ */
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_products_ajax_search_backend_execute',
+    function ($searchParams) {
+        \QUI\Rights\Permission::checkPermission(
+            SearchHandler::PERMISSION_BACKEND_EXECUTE
+        );
+
+        $BackEndSearch = SearchHandler::getBackendSearch();
+        $searchParams  = \QUI\Utils\Security\Orthos::clearArray(
+            json_decode($searchParams, true)
+        );
+
+        return $BackEndSearch->search($searchParams);
+    },
+    array('searchParams'),
+    'Permission::checkAdminUser'
+);
---
+++ src/QUI/ERP/Products/Console/GenerateProductAttributeListTags.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file contains \Hklused\Machines\Patch
+ */
+
+namespace QUI\ERP\Products\Console;
+
+use QUI;
+
+/**
+ * Console tool for HKL used patches
+ *
+ * @author www.pcsg.de (Patrick Müller)
+ */
+class GenerateProductAttributeListTags extends QUI\System\Console\Tool
+{
+    /**
+     * Konstruktor
+     */
+    public function __construct()
+    {
+        $this->setName('products:generateProductAttributeListTags')
+            ->setDescription(
+                'Generate tags for projects/products from every product attribute list option'
+            );
+    }
+
+    /**
+     * Execute the console tool
+     */
+    public function execute()
+    {
+        QUI\Rights\Permission::isAdmin();
+        QUI\ERP\Products\Crons::generateProductAttributeListTags();
+    }
+}
