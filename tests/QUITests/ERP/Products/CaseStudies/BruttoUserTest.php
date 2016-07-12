1a596f96af4ed36b698942c80811c307ac609483
Henning Leutz <leutz@pcsg.de>
2016-07-12 11:35:59 +0200

    test: require-dev

--- tests/QUITests/ERP/Products/CaseStudies/BruttoUserTest.php
+++ tests/QUITests/ERP/Products/CaseStudies/BruttoUserTest.php
@@ -9,5 +9,18 @@ use QUI;
  */
 class BruttoUserTest extends \PHPUnit_Framework_TestCase
 {
+    public function testCasetStudieBrutto()
+    {
+        $Brutto = $this->userDummy();
 
+    }
+
+
+    protected function userDummy()
+    {
+        return $this->getMockBuilder('\QUI\Users\User')
+            ->setMethods(array('debug', 'info'))
+            ->getMock();
+
+    }
 }
