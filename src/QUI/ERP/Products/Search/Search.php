d9d15dbf442e61d4b8b87855412ed57e17892dac
Patrick Müller <p.mueller@pcsg.de>
2016-08-08 14:12:00 +0200

    fix: spalte kann nur sortiert werden, wenn sie auch durchsucht werden darf

--- src/QUI/ERP/Products/Search/Search.php
+++ src/QUI/ERP/Products/Search/Search.php
@@ -686,6 +686,11 @@ abstract class Search extends QUI\QDOM
 
                 try {
                     $OrderField = Fields::getField($orderFieldId);
+
+                    if (!$this->canSearchField($OrderField)) {
+                        throw new QUI\Exception();
+                    }
+
                     $order     .= ' ' . SearchHandler::getSearchFieldColumnName($OrderField);
                 } catch (\Exception $Exception) {
                     // if field does not exist or throws some other kind of error - it is not searchable
