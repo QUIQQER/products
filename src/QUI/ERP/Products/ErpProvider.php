6060cd7866c1ce7d3c1fcfc3227de8469e9f2443
Henning Leutz <leutz@pcsg.de>
2017-03-08 08:39:49 +0100

    fix: ERP Provider -> Shop Menü erweitert

---
+++ src/QUI/ERP/Products/ErpProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file contains QUI\ERP\Products\EventHandling
+ */
+namespace QUI\ERP\Products;
+
+use QUI\ERP\Api\AbstractErpProvider;
+
+/**
+ * Class ErpProvider
+ * Produkte ERP Provider -> erweitert das ERP Shop Panel
+ *
+ * @package QUI\ERP\Products
+ */
+class ErpProvider extends AbstractErpProvider
+{
+    /**
+     * @return array
+     */
+    public static function getMenuItems()
+    {
+        $menu = array();
+
+        $menu[] = array(
+            'icon'  => 'fa fa-shopping-bag',
+            'text'  => array('quiqqer/products', 'menu.erp.products.products.title'),
+            'panel' => 'package/quiqqer/products/bin/controls/products/Panel'
+        );
+
+        $menu[] = array(
+            'icon'  => 'fa fa-sitemap',
+            'text'  => array('quiqqer/products', 'menu.erp.products.categories.title'),
+            'panel' => 'package/quiqqer/products/bin/controls/categories/Panel'
+        );
+
+        $menu[] = array(
+            'icon'  => 'fa fa-file-text-o',
+            'text'  => array('quiqqer/products', 'menu.erp.products.fields.title'),
+            'panel' => 'package/quiqqer/products/bin/controls/fields/Panel'
+        );
+
+        return $menu;
+    }
+}
