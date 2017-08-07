fc146afada02f28aeec00335d571a9c4fadf8a66
Henning Leutz <leutz@pcsg.de>
2017-08-07 15:20:17 +0200

    fix: quiqqer/package-piwik#4 - kompriierung der piwik.js

---
+++ bin/Stats.js
@@ -0,0 +1,14 @@
+/**
+ * Piwik Tracker
+ *
+ * @module package/quiqqer/products/bin/Stats
+ * @author www.pcsg.de (Henning Leutz)
+ *
+ * @require package/quiqqer/products/bin/classes/frontend/Stats
+ */
+define('package/quiqqer/products/bin/Stats', [
+    'package/quiqqer/products/bin/classes/frontend/Stats'
+], function (Piwik) {
+    "use strict";
+    return new Piwik();
+});
