f12938d5f382c2b2cbc1731861805d46e6bb0efe
Henning Leutz <leutz@pcsg.de>
2019-05-28 12:07:49 +0200

    feat: product url check integrated

--- src/QUI/ERP/Products/Product/Model.php
+++ src/QUI/ERP/Products/Product/Model.php
@@ -963,6 +963,10 @@ class Model extends QUI\QDOM
     {
         QUI\Permissions\Permission::checkPermission('product.edit');
 
+        // check url
+        $this->checkProductUrl($fieldData);
+
+
         $categoryIds = [];
         $categories  = $this->getCategories();
 
@@ -1025,6 +1029,87 @@ class Model extends QUI\QDOM
         QUI\Cache\Manager::clear('quiqqer/products/'.$this->getId());
 
         QUI::getEvents()->fireEvent('onQuiqqerProductsProductSave', [$this]);
+    }
+
+    /**
+     * Check if the product url already exists in the category
+     *
+     * @param array $fieldData
+     * @throws Exception
+     */
+    protected function checkProductUrl($fieldData)
+    {
+        // check url
+        $urlField = \array_filter($fieldData, function ($field) {
+            return $field['id'] === Fields::FIELD_URL;
+        });
+
+        $urlField = \array_values($urlField);
+        $urls     = [];
+
+        if (isset($urlField[0])) {
+            $urls = $urlField[0]['value'];
+        }
+
+        $categoryId    = $this->getCategory()->getId();
+        $urlCacheField = 'F'.Fields::FIELD_URL;
+        $table         = QUI\ERP\Products\Utils\Tables::getProductCacheTableName();
+
+        $where = [];
+        $binds = [];
+        $i     = 0;
+
+        foreach ($urls as $lang => $url) {
+            if (empty($url)) {
+                continue;
+            }
+
+            $binds[':lang'.$i]     = $lang;
+            $binds[':url'.$i]      = $url;
+            $binds[':category'.$i] = '%,'.$categoryId.',%';
+
+            $where[] = "(F19 LIKE :url{$i} AND lang LIKE :lang{$i} AND category LIKE :category{$i})";
+            $i++;
+        }
+
+        if (empty($where)) {
+            return;
+        }
+
+        $where = \implode(' OR ', $where);
+
+        $query = "
+            SELECT id, {$urlCacheField} 
+            FROM {$table}
+            WHERE {$where}
+        ";
+
+        $PDO       = QUI::getDataBase()->getPDO();
+        $Statement = $PDO->prepare($query);
+
+        foreach ($binds as $bind => $value) {
+            $Statement->bindValue($bind, $value, \PDO::PARAM_STR);
+        }
+
+        $Statement->execute();
+        $result = $Statement->fetchAll();
+
+        // no results, all is fine
+        if (empty($result)) {
+            return;
+        }
+
+
+        foreach ($result as $entry) {
+            if ($entry === $this->getId()) {
+                continue;
+            }
+
+            throw new Exception([
+                'quiqqer/products',
+                'exception.url.already.exists'
+            ]);
+        }
     }
 
     /**
