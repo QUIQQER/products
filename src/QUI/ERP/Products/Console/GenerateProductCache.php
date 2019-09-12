2c144fa982dca0dbc92040cbadb4952bc6f0d1c3
Henning Leutz <leutz@pcsg.de>
2019-09-12 09:45:29 +0200

    feat: console -> product cache

---
+++ src/QUI/ERP/Products/Console/GenerateProductCache.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file contains \QUI\ERP\Products\Console
+ */
+
+namespace QUI\ERP\Products\Console;
+
+use QUI;
+use QUI\ERP\Products\Handler\Products;
+
+/**
+ * Console tool for HKL used patches
+ *
+ * @author www.pcsg.de (Patrick Müller)
+ */
+class GenerateProductCache extends QUI\System\Console\Tool
+{
+    /**
+     * Konstruktor
+     */
+    public function __construct()
+    {
+        $this->setName('products:generate-product-cache')
+            ->setDescription(
+                'Generate the primary product cache'
+            );
+    }
+
+    /**
+     * Execute the console tool
+     */
+    public function execute()
+    {
+        $productIds = Products::getProductIds();
+
+        $i = 0;
+
+        foreach ($productIds as $productId) {
+            try {
+                $Product = Products::getProduct($productId);
+                $Product->getView();
+            } catch (QUI\Exception $Exception) {
+                QUI\System\Log::addInfo($Exception->getMessage());
+            }
+
+            if ($i % 100 === 0) {
+                $this->writeLn('- '.$i);
+            }
+
+            $i++;
+        }
+    }
+}
