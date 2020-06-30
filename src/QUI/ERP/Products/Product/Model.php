6a9e2f8a482c6c2fe5c93f3761a0a1a9847ed16a
Patrick Müller <p.mueller@pcsg.de>
2020-06-30 15:46:22 +0200

    feat: EditUser parameter for Product->activate() and Product->deactivate() refactor: Product->save() -> changed param from PermissionUser to EditUser

--- src/QUI/ERP/Products/Product/Model.php
+++ src/QUI/ERP/Products/Product/Model.php
@@ -1108,48 +1108,48 @@ class Model extends QUI\QDOM
     /**
      * Alias for save()
      *
-     * @param QUI\Interfaces\Users\User $PermissionUser (optional) - The user that executes the operation
+     * @param QUI\Interfaces\Users\User $EditUser (optional) - The user that executes the operation
      * @return void
      *
      * @throws QUI\Exception
      */
-    public function update($PermissionUser = null)
+    public function update($EditUser = null)
     {
-        $this->save($PermissionUser);
+        $this->save($EditUser);
     }
 
     /**
      * save / update the product data
      *
-     * @param QUI\Interfaces\Users\User $PermissionUser (optional) - The user that executes the operation
+     * @param QUI\Interfaces\Users\User $EditUser (optional) - The user that executes the operation
      * @return void
      *
      * @throws QUI\ERP\Products\Product\Exception
      * @throws QUI\Permissions\Exception
      * @throws QUI\Exception
      */
-    public function save($PermissionUser = null)
+    public function save($EditUser = null)
     {
-        $this->productSave($this->getFieldData(), $PermissionUser);
+        $this->productSave($this->getFieldData(), $EditUser);
     }
 
     /**
      * Internal saving method
      *
      * @param array $fieldData - field data
-     * @param QUI\Interfaces\Users\User $PermissionUser (optional) - The user that executes the operation
+     * @param QUI\Interfaces\Users\User $EditUser (optional) - The user that executes the operation
      *
      * @throws QUI\Permissions\Exception
      * @throws QUI\Exception
      * @throws Exception
      */
-    protected function productSave($fieldData, $PermissionUser = null)
+    protected function productSave($fieldData, $EditUser = null)
     {
-        if (empty($PermissionUser)) {
-            $PermissionUser = QUI::getUserBySession();
+        if (empty($EditUser)) {
+            $EditUser = QUI::getUserBySession();
         }
 
-        QUI\Permissions\Permission::checkPermission('product.edit', $PermissionUser);
+        QUI\Permissions\Permission::checkPermission('product.edit', $EditUser);
 
         // cleanup urls
         $urlField = \array_filter($fieldData, function ($field) {
@@ -1247,7 +1247,7 @@ class Model extends QUI\QDOM
                     'category'    => $mainCategory,
                     'fieldData'   => \json_encode($fieldData),
                     'permissions' => \json_encode($this->permissions),
-                    'e_user'      => $PermissionUser->getId(),
+                    'e_user'      => $EditUser->getId(),
                     'e_date'      => $this->getAttribute('e_date')
                 ],
                 ['id' => $this->getId()]
@@ -2118,11 +2118,18 @@ class Model extends QUI\QDOM
     /**
      * Deactivate the product
      *
+     * @param QUI\Interfaces\Users\User $EditUser (optional) - The user that executes the operation
+     * @return void
+     *
      * @throws QUI\Exception
      */
-    public function deactivate()
+    public function deactivate($EditUser = null)
     {
-        QUI\Permissions\Permission::checkPermission('product.activate');
+        if (empty($EditUser)) {
+            $EditUser = QUI::getUserBySession();
+        }
+
+        QUI\Permissions\Permission::checkPermission('product.activate', $EditUser);
 
         $this->active = false;
 
@@ -2147,13 +2154,20 @@ class Model extends QUI\QDOM
     /**
      * Activate the product
      *
+     * @param QUI\Interfaces\Users\User $EditUser (optional) - The user that executes the operation
+     * @return void
+     *
      * @throws QUI\ERP\Products\Product\Exception
      * @throws QUI\Permissions\Exception
      * @throws QUI\Exception
      */
-    public function activate()
+    public function activate($EditUser = null)
     {
-        QUI\Permissions\Permission::checkPermission('product.activate');
+        if (empty($EditUser)) {
+            $EditUser = QUI::getUserBySession();
+        }
+
+        QUI\Permissions\Permission::checkPermission('product.activate', $EditUser);
 
         // exist a main category?
         $Category = $this->getCategory();
