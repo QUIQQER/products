b38236fc94de7b5bc154a71aab75366253c3cc2e
Patrick Müller <p.mueller@pcsg.de>
2021-06-16 13:42:52 +0200

    refactor: do not set ownField and unassigned status when chaging field options

--- ajax/fields/setProductFieldArray.php
+++ ajax/fields/setProductFieldArray.php
@@ -15,12 +15,18 @@ use QUI\ERP\Products\Handler\Products;
 QUI::$Ajax->registerFunction(
     'package_quiqqer_products_ajax_fields_setProductFieldArray',
     function ($fieldId) {
-        $Field        = Fields::getField($fieldId);
-        $productArray = $Field->toProductArray();
+        /*
+         * unassigned and ownField attributes are not explicit attributes
+         * of the field itself but of the product in conjunction with a field.
+         *
+         * thus it must not be set here.
+         */
+//        $Field        = Fields::getField($fieldId);
+//        $productArray = $Field->toProductArray();
 
-        Fields::setFieldAttributesToProducts($fieldId, [
-            'unassigned' => $productArray['unassigned'],
-            'ownField'   => $productArray['ownField']
+        Fields::setFieldAttributesToProducts((int)$fieldId, [
+//            'unassigned' => $productArray['unassigned'],
+//            'ownField'   => $productArray['ownField']
         ]);
     },
     ['fieldId'],
