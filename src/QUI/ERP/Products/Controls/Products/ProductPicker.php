151bf3ce2f3711288d1b52411ebfb8d0cf9e0f6b
Henning <leutz@pcsg.de>
2025-06-03 15:24:02 +0200

    feat: add new ProductPicker component
    This commit introduces the new ProductPicker component. This component displays a set of products
    to the user and allows them to pick a product based on various options.
    
    The changes introduce new HTML, CSS, and JavaScript files for the component, as well as a new PHP
    class to handle server-side logic. The component is designed to be flexible, supporting an
    arbitrary number of products and options. The appearance of options can be customized by set them
    as 'select' or 'radio'.
    
    The PHP class performs essential tasks such as fetching the correct products based on user
    selection, and calculating their prices for the current user session.
    
    The CSS styles the component layout and appearance, and the HTML structures the component markup.
    JavaScript handles user interactions, performing tasks such as responding to user input,
    dynamically adjusting the display based on the current state, and making server requests to fetch
    updated data when necessary.

---
+++ src/QUI/ERP/Products/Controls/Products/ProductPicker.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace QUI\ERP\Products\Controls\Products;
+
+use QUI;
+
+class ProductPicker extends QUI\Control
+{
+    public function __construct($params = [])
+    {
+        parent::__construct($params);
+
+        $this->setAttributes([
+            'sheetOptionsStyle' => 'select', // select, radio
+            'sheetOptions' => [],
+            'sheets' => []
+        ]);
+
+        $this->setJavaScriptControl(
+            'package/quiqqer/products/bin/controls/frontend/products/ProductPicker'
+        );
+
+        $this->addCSSFile(dirname(__FILE__) . '/ProductPicker.css');
+    }
+
+    public function getBody(): string
+    {
+        $Engine = QUI::getTemplateManager()->getEngine();
+        $sheetOptions = $this->getAttribute('sheetOptions');
+        $sheets = $this->getAttribute('sheets');
+
+        if (!is_array($sheetOptions)) {
+            $sheetOptions = [];
+        }
+
+        if (!is_array($sheets)) {
+            $sheets = [];
+        }
+
+        $SessionUser = QUI::getUserBySession();
+
+        // products
+        foreach ($sheets as $k => $sheet) {
+            foreach ($sheet['options'] as $interval => $productId) {
+                    $Product = QUI\ERP\Products\Handler\Products::getProduct($productId);
+                    $Price = $Product->getPrice($SessionUser);
+
+                    $sheets[$k]['options'][$interval] = [
+                        'id' => $productId,
+                        'Product' => $Product,
+                        'Price' => $Price
+                    ];
+            }
+        }
+
+
+
+        $Engine->assign([
+            'this' => $this,
+            'sheetOptionsStyle' => $this->getAttribute('sheetOptionsStyle'),
+            'sheetOptions' => $sheetOptions,
+            'sheets' => $sheets
+        ]);
+
+        return $Engine->fetch(dirname(__FILE__) . '/ProductPicker.html');
+    }
+}
