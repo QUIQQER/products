ae82ae94f67962a3d7aef619a9925142013fc351
Henning <leutz@pcsg.de>
2025-11-02 06:35:15 +0100

    fix: remove unnecessary phpstan ignore
    In the Products.php file, the unnecessary @phpstan-ignore-next-line has been removed. This was
    causing undesired silencing of potential issues. Now, phpstan will properly check this line for any
    possible issues.

--- src/QUI/ERP/Products/Utils/Products.php
+++ src/QUI/ERP/Products/Utils/Products.php
@@ -91,7 +91,6 @@ class Products
         $priceValue = $PriceField->getValue();
 
         // $priceValue may be NULL or empty string; in these cases, consider the default price field value as not set.
-        // @phpstan-ignore-next-line
         if (empty($priceValue) && $priceValue != 0) {
             $priceValue = null;
         }
